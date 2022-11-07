b9369af406c85039a8392f8724a5e403d48cfbd6
Nicolas Amadori - nicolas.amadori@studio.unibo.it <nicolas.amadori@studio.unibo.it>
2022-11-07 13:02:05 +0100

    Punto 1 e 2 dell'esercizio 61 terminati. Iniziato il punto 3

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,6 +9,43 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    public enum Month {
+        JANUARY("JANUARY",31),
+        FEBRUARY("FEBRUARY",28), 
+        MARCH("MARCH", 31), 
+        APRIL("APRIL", 30), 
+        MAY("MAY", 31), 
+        JUNE("JUNE", 30), 
+        JULY("JULY", 31), 
+        AUGUST("AUGUST", 31), 
+        SEPTEMBER("SEPTEMBER", 30), 
+        OCTOBER("OCTOBER", 31), 
+        NOVEMBER("NOVEMBER", 30), 
+        DECEMBER("DECEMBER", 31);
+
+        private final String name;
+        private final int daysNumber;
+
+        private Month(final String name, final int daysNumber) {
+            this.name = name;
+            this.daysNumber = daysNumber;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getDaysNumber() {
+            return this.daysNumber;
+        }
+
+        public Month fromString(String name){
+            for (Month m : this.values()) {
+                if(m.getName().contains(name.toUpperCase())) return m;
+            }
+        }
+    }
+
     @Override
     public Comparator<String> sortByDays() {
         return null;
